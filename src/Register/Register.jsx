cfcd00fd4bc12453cd50e433f1b1121a72c07650
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-09-30 14:53:45 +0600

    Initial project setup

---
+++ src/Register/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h3>this is register</h3>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/Route/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default Router;
